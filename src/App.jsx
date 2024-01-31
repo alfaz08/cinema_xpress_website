d9ed012dbf4b5c3c3129f4cceb519f07c66789bb
Alfaz Hossain <alfaz.cse@gmail.com>
2024-01-31 11:18:04 +0600

    header and sidebar is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Header from "./components/Header/Header"
+import Sidebar from "./components/Sidebar/Sidebar"
 
 
 function App() {
   
   return (
     <>
-      <h2>Hello</h2>
+      <Header></Header>
+      <main>
+		<div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+     <Sidebar></Sidebar>
+    </div>
+    </main>
     </>
   )
 }
