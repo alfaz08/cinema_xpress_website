f975a34b1e7f79f20aa053009f939d8034e5639c
Alfaz Hossain <alfaz.cse@gmail.com>
2024-01-31 17:04:48 +0600

    cart open and close is added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,25 @@ import Logo from '/public/assets/logo.svg'
 import Ring from '/public/assets/ring.svg'
 import Moon from '/public/assets/icons/moon.svg'
 import Shop from '/public/assets/shopping-cart.svg'
+import { useState } from 'react'
+import CardDetails from '../../cine/CartDetails'
 
 export default function Header(){
+
+  const [showCart,setShowCart] =useState(false)
+
+  const handleCartShow=()=>{
+		setShowCart(!showCart)
+	}
+
+
+
     return (
         <div>
           <header>
+						{
+							showCart && <CardDetails onChange={handleCartShow} ></CardDetails>
+						}
 		<nav className="container flex items-center justify-between space-x-10 py-6">
 			<a href="index.html">
 				<img src={Logo} width="139" height="26" alt="" />
@@ -25,7 +39,7 @@ export default function Header(){
 				</li>
 				<li>
 					<a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-						<img src={Shop} width="24" height="24" alt="" />
+						<img src={Shop} onClick={handleCartShow}  width="24" height="24" alt="" />
 					</a>
 				</li>
 			</ul>
